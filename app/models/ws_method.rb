90a85b74ab9054b4453f30f7b5ef87af62750274
sudakov <vsudakov@bk.ru>
2015-08-30 22:42:37 +0300

    bool

--- app/models/ws_method.rb
+++ app/models/ws_method.rb
@@ -1,3 +1,33 @@
+class TrueClass
+	def impl( other )
+	  other 
+	end
+	def equviv( other )
+	  other
+	end
+	def pirs( other )
+	  false 
+	end
+	def sheph( other )
+	  not other
+	end
+end
+
+class FalseClass
+	def impl( other )
+	  true
+	end
+	def equviv( other )
+	  not other 
+	end
+	def pirs( other )
+	  not other 
+	end
+	def sheph( other )
+	  true
+	end
+end
+
 class WsMethod < ActiveRecord::Base
   nilify_blanks
   
