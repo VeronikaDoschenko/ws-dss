6ebcee78b80b66fb0cd30fd5dfd6a1021d8de37c
sudakov <vsudakov@bk.ru>
2016-04-27 18:13:30 +0300

    values

--- app/models/ws_model_run.rb
+++ app/models/ws_model_run.rb
@@ -27,37 +27,56 @@ class WsModelRun < ActiveRecord::Base
         system "rake ws_dss:process_ws_model_run[#{mr.id}] --trace 2>&1 >> #{Rails.root.join('log',"#{Rails.env}.log")} &"
       when 7
         can_run = true
-        mr.param_values.where("param_values.old_value is null and
-          exists(select 1 from ws_param_models where param_values.ws_param_id = ws_param_models.ws_param_id and
-                                                     ws_model_runs.ws_model_id = ws_param_models.ws_model_id and
-                                                     ws_param_models.is_required = 1
+        mr.ws_param_values.where("exists(select 1
+                                      from   ws_param_models
+                                      where  ws_param_values.ws_param_id  = ws_param_models.ws_param_id and
+                                             #{mr.ws_model_id} = ws_param_models.ws_model_id and
+                                             ws_param_models.is_required = true
                 )                       
         ").each do |pv|
-          can_run = false
-          # find source
           if pv.ws_set_model_run.nil?
-            mr.trace += "\nCan not find source for param #{pv.ws_param.name}"
-            return
-          end
-          pv.ws_set_model_run.ws_model_runs.each do |smr|
-            case smr.ws_model_status_id
-            when 1
-              smr.update(ws_model_status_id: 7)
-            when 4
-              # copy value
-              value = []
-              pv.source_ws_params.each do |sp| 
-                spv = smr.param_values.find_by_ws_param_id(sp.id)
-                if spv.nil? or spv.new_value.blank?
-                  mr.trace += "\nCan not find source for param #{pv.ws_param.name} in #{smr.name}"
-                  return
+            if pv.old_value.blank?
+              mr.trace += "\nCan not find source for param #{pv.ws_param.name}"
+              can_run = false
+            end
+          elsif pv.source_ws_params.size == 0
+            mr.trace += "\nCan not find source params for param #{pv.ws_param.name}"
+            can_run = false
+          elsif pv.ws_set_model_run.ws_model_runs.size == 0
+            mr.trace += "\nCan not find runs in set #{pv.ws_set_model_run.name} for param #{pv.ws_param.name}"
+            can_run = false
+          else
+            pov = Array.new(pv.source_ws_params.size) {Array.new(pv.ws_set_model_run.ws_model_runs.size)}
+            pv.source_ws_params.each_with_index do |sp, j|
+              pv.ws_set_model_run.ws_model_runs.each_with_index do |smr,i|
+                case smr.ws_model_status_id
+                when 1
+                  smr.update(ws_model_status_id: 7)
+                  can_run = false
+                when 4
+                  spv = smr.param_values.find_by_ws_param_id(sp.id)
+                  if spv.nil? or spv.new_value.blank?
+                    mr.trace += "\nCan not find source for param #{pv.ws_param.name} in #{smr.name}"
+                    can_run = false
+                  else
+                    pov[j][i]=spv.new_value
+                  end
+                when 5
+                  mr.trace += "\nError value in source model run #{smr.name} for param #{pv.ws_param.name}"
+                  can_run = false
+                when 6
+                  mr.trace += "\nError in source model run #{smr.name} for param #{pv.ws_param.name}"
+                  can_run = false
                 end
-                value << spv.new_value
               end
             end
+            pv.update( old_value: pov.to_s ) if can_run
           end
-          
         end
+        unless mr.trace.blank?
+            self.update_columns(trace: mr.trace)
+        end
+        
         mr.update(ws_model_status_id: 2) if can_run        
       when 4
         # transfer param_values if all set
