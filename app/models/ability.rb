d54c0177e706d0f55e85e73b6a0f7e853774d1a1
sudakov <vsudakov@bk.ru>
2016-08-22 18:09:30 +0300

    permission flex ability

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -13,15 +13,17 @@ class Ability
       can :index, :ws_set_model_runs
       can :index, :ws_param_values
       
-      can :update, WsModelRun, :user_id => user.id
-      can :read, WsModelRun, :user_id => user.id
-      
       can [:update,:destroy,:read], WsModel, :user_id => user.id
-      can :read, WsModel do |ws_model|
-        a = user.roles.collect{|x| x.name} + %w[ public ]
-        b = ws_model.roles.collect{|x| x.name}
-        (a & b).size > 0
-      end
+      can :read, WsModel,
+          :id => ActiveRecord::Base.connection.execute(
+                 "select distinct rc.roleable_id from royce_connector rc
+                  where rc.roleable_type = 'WsModel' and 
+                        ( rc.role_id = (select id from royce_role where name = 'public') or 
+                          rc.role_id in (select rcu.role_id
+                                         from royce_connector rcu
+                                         where rcu.roleable_type = 'User' and
+                                         rcu.roleable_id=#{user.id})
+                        )").collect{|x| x["roleable_id"].to_i}   
       
       can [:update,:destroy,:read], WsParam, :user_id => user.id
       can :read, WsParam do |ws_param|
@@ -35,12 +37,17 @@ class Ability
  
       can :create, WsModelRun
       can [:update,:destroy,:read], WsModelRun, :user_id => user.id
-      #can :read, WsModelRun do |ws_model_run|
-      #  a = user.roles.collect{|x| x.name} + %w[ public ]
-      #  b = ws_model_run.roles.collect{|x| x.name}
-      #  (a & b).size > 0
-      #end
-            
+      can :read, WsModelRun,
+          :id => ActiveRecord::Base.connection.execute(
+                 "select distinct rc.roleable_id from royce_connector rc
+                  where rc.roleable_type = 'WsModelRun' and 
+                        ( rc.role_id = (select id from royce_role where name = 'public') or 
+                          rc.role_id in (select rcu.role_id
+                                         from royce_connector rcu
+                                         where rcu.roleable_type = 'User' and
+                                         rcu.roleable_id=#{user.id})
+                        )").collect{|x| x["roleable_id"].to_i}
+          
       can :create, WsSetModelRun 
       can [:update,:destroy,:read], WsSetModelRun, :user_id => user.id 
       
