3ad346908f6a2aa257ea52db510ab4e6a0c3b7c3
sudakov <vsudakov@bk.ru>
2016-12-29 15:10:05 +0300

    size

--- app/models/document.rb
+++ app/models/document.rb
@@ -34,8 +34,8 @@ private
   end
   NUM_BYTES_IN_MEGABYTE = 1048576
   def file_size
-    if @file and (@file.size.to_f / NUM_BYTES_IN_MEGABYTE) > 4
-      errors.add(:file, 'File size cannot be over 4 megabyte.')
+    if @file and (@file.size.to_f / NUM_BYTES_IN_MEGABYTE) > 20
+      errors.add(:file, 'File size cannot be over 20 megabyte.')
     end
   end
 end
