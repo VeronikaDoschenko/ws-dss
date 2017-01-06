58249e7e4bd7ad2062b02570fd8b536e55acad35
sudakov <vsudakov@bk.ru>
2017-01-06 21:01:07 +0300

    fix

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -3,7 +3,4 @@ Recaptcha.configure do |config|
   config.secret_key = ENV['RECAPCHA_PRIVATE_KEY']
   # Uncomment the following line if you are using a proxy server:
   # config.proxy = 'http://myproxy.com.au:8080'
-  # Uncomment if you want to use the newer version of the API,
-  # only works for versions >= 0.3.7:
-  # config.api_version = 'v1'
 end
