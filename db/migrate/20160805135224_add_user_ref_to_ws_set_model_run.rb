fa6928ffbf51aa2391db0ad60aa94283098a1899
sudakov <vsudakov@bk.ru>
2017-01-14 16:59:11 +0300

    fix

--- db/migrate/20160805135224_add_user_ref_to_ws_set_model_run.rb
+++ db/migrate/20160805135224_add_user_ref_to_ws_set_model_run.rb
@@ -1,7 +1,7 @@
 class AddUserRefToWsSetModelRun < ActiveRecord::Migration
   def change
     add_reference :ws_set_model_runs, :user, index: true, foreign_key: true
-    user_id = User.find_by_id(1).id
+    user_id = User.find_by_id(1).try(:id)
     if user_id
       WsSetModelRun.update_all "user_id = #{user_id}" 
       WsModelRun.where( user_id: nil).update_all("user_id =  #{user_id}")
