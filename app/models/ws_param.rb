d951b89fa776e5d494162727b966757c3fa49d28
begotten63 <begotten63@gmail.com>
2017-05-03 05:22:39 +0300

    add description associations for ws_model,ws_params,ws_set_model_run

--- app/models/ws_param.rb
+++ app/models/ws_param.rb
@@ -1,10 +1,9 @@
 class WsParam < ActiveRecord::Base
   nilify_blanks
   belongs_to :user
+  has_many :descriptions, as: :rec, dependent: => :destroy
   has_many :ws_param_values
-  
   has_many :ws_param_values_params
-  
   has_many :target_ws_param_values, class_name: 'WsParamValue', through: :ws_param_values_params
   
   accepts_nested_attributes_for :ws_param_values_params, :allow_destroy => true
--- app/models/ws_set_model_run.rb
+++ app/models/ws_set_model_run.rb
@@ -1,5 +1,6 @@
 class WsSetModelRun < ActiveRecord::Base
   nilify_blanks
+  has_many :descriptions, as: :rec, dependent: => :destroy
   has_many :ws_model_runs_set_model_runs, :dependent => :destroy
   has_many :ws_model_runs, through: :ws_model_runs_set_model_runs
   has_many :ws_param_values
